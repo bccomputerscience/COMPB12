ff834aae2fbb4fb9b06c4a0dff989fb2a7f362e9
Eddie Rangel <hacker@eddierangel.com>
2017-02-10 12:27:20 -0800

    Update formatting

--- examples/FundamentalDataTypes.cpp
+++ examples/FundamentalDataTypes.cpp
@@ -6,15 +6,26 @@
 
 #include<iostream>
 #include<climits>
+#include<cfloat>
 #include <iomanip>
 using namespace std;
 
 int main()
 {
-    cout << "Fundamental Data Types in C++ 11" << endl;
+    cout << setw(50) << "================================" << endl
+         << setw(50) << "Fundamental Data Types in C++ 11" << endl
+         << setw(50) << "================================" << endl;
 
-    cout << "Data Type" << setw(10) << "Bytes" << setw(15) << "Minimum" << setw(15) << "Maximum" << endl;
-    cout << "Short" << setw(14) << sizeof(short) << setw(15) << SHRT_MIN << setw(15) << SHRT_MAX << endl;
-    cout << "Int" << setw(16) << sizeof(int) << setw(15) << INT_MIN << setw(15) << INT_MAX <<endl;
+    cout << "Data Type" << setw(10) << "Bytes" << setw(21) << "Minimum" << setw(21) << "Maximum" << endl
+         << "short" << setw(14) << sizeof(short) << setw(21) << SHRT_MIN << setw(21) << SHRT_MAX << endl
+         << "int" << setw(16) << sizeof(int) << setw(21) << INT_MIN << setw(21) << INT_MAX <<endl
+         << "unsigned int" << setw(7) << sizeof(unsigned int) << setw(21) << 0 << setw(21) << UINT_MAX <<endl
+         << "long int" << setw(11) << sizeof(long int) << setw(21) << LONG_MIN << setw(21) << LONG_MAX <<endl
+         << "float" << setw(14) << sizeof(float) << setw(21) << FLT_MIN  << setw(21) << FLT_MAX  <<endl
+         << "double" << setw(13) << sizeof(double) << setw(21) <<  DBL_MIN  << setw(21) <<  DBL_MAX  <<endl
+         << "long double" << setw(8) << sizeof(long double) << setw(21) << LDBL_MIN << setw(21) << LDBL_MAX <<endl
+         << endl;
+
+    cout << "*These values are platform dependent." << endl;
 
 }
