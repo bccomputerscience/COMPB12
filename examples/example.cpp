3541b675747dfffa45c17a137a5e42a78da68ab0
Eddie Rangel <hacker@eddierangel.com>
2018-08-29 10:28:04 -0700

    Adding template file

---
+++ examples/example.cpp
@@ -0,0 +1,24 @@
+/*
+    Eddie Rangel
+    COMP B12
+    8-29-2018
+    example.cpp
+    Description: Provide an example of a C++ file that can be used as a template
+
+    Usage: 
+    g++ -Wall example.cpp -o example
+
+    ./example
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Template for COMP B12! " << endl;
+    cout << "Hack The Planet " << endl;
+
+    return 0;
+}
